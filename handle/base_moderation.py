5791be6a9fa3dcc9ba78311e4a9b6fa06345b1c0
Ian Mackinnon <imackinnon@gmail.com>
2014-11-10 14:27:09 +0100

    Add more information to moderation queue page.

--- handle/base_moderation.py
+++ handle/base_moderation.py
@@ -11,46 +11,49 @@ from model_v import Org_v, Event_v, Address_v, Contact_v, \
 
 
 
-def get_pending_entity_id(orm,
-                          Entity_v,
-                          Entity,
-                          ):
-        latest = orm.query(Entity_v.entity_id.label("entity_id"), Entity_v.moderation_user_id) \
-            .order_by(Entity_v.entity_v_id.desc()) \
-            .subquery()
+def get_pending_entity_id(orm, Entity_v, Entity, desc_attr):
+    latest = orm.query(Entity_v.entity_id.label("entity_id"), getattr(Entity_v, desc_attr).label("desc"), Entity_v.moderation_user_id) \
+        .order_by(Entity_v.entity_v_id.desc()) \
+        .subquery()
 
-        latest = orm.query(latest.c.entity_id, latest.c.moderation_user_id) \
-            .group_by(latest.c.entity_id) \
-            .subquery()
+    latest = orm.query(latest.c.entity_id, latest.c.desc, latest.c.moderation_user_id) \
+        .group_by(latest.c.entity_id) \
+        .subquery()
 
-        results = orm.query(latest.c.entity_id) \
-            .join((User, User.user_id == latest.c.moderation_user_id)) \
-            .outerjoin((Entity, Entity.entity_id == latest.c.entity_id)) \
-            .add_columns(Entity.entity_id) \
-            .filter(User.moderator == False) \
-            .limit(20)
+    results = orm.query(latest.c.entity_id, latest.c.desc) \
+        .join((User, User.user_id == latest.c.moderation_user_id)) \
+        .outerjoin((Entity, Entity.entity_id == latest.c.entity_id)) \
+        .add_columns(Entity.entity_id, getattr(Entity, desc_attr), User.name) \
+        .filter(User.moderator == False) \
+        .limit(20)
 
-        return [(id_1, bool(id_2)) for (id_1, id_2) in results]
+    results = list(results)
+    for i, row in enumerate(results):
+        row = list(row)
+        row[2] = bool(row[2])
+        results[i] = row
+
+    return results
 
 
 
 def get_pending_org_id(orm):
-    return get_pending_entity_id(orm, Org_v, Org)
+    return get_pending_entity_id(orm, Org_v, Org, "name")
 
 
 
 def get_pending_event_id(orm):
-    return get_pending_entity_id(orm, Event_v, Event)
+    return get_pending_entity_id(orm, Event_v, Event, "name")
 
 
 
 def get_pending_address_id(orm):
-    return get_pending_entity_id(orm, Address_v, Address)
+    return get_pending_entity_id(orm, Address_v, Address, "postal")
 
 
 
 def get_pending_contact_id(orm):
-    return get_pending_entity_id(orm, Contact_v, Contact)
+    return get_pending_entity_id(orm, Contact_v, Contact, "text")
 
 
 
@@ -77,71 +80,74 @@ def has_address_not_found(orm):
 
 
 
-def get_pending_org_address_id(orm):
-    address_id_list = dict(get_pending_entity_id(orm, Address_v, Address))
+def dict_by_first(list_):
+    dict_ = {}
+    for row in list_:
+        dict_[row[0]] = row
+    return dict_
 
-    query = orm.query(org_address_v.c.org_id, org_address_v.c.address_id) \
-        .filter(org_address_v.c.address_id.in_(address_id_list.keys())) \
-        .outerjoin((Org, Org.org_id == org_address_v.c.org_id)) \
-        .add_columns(Org.org_id) \
+
+
+def get_pending_parent_entity_id(
+        orm,
+        get_pending_entity_id,
+        parent_entity_v,
+        Parent, parent_id, parent_desc, entity_id, 
+):
+    entity_id_list = dict_by_first(get_pending_entity_id(orm))
+
+    query = orm.query(getattr(parent_entity_v.c, parent_id), getattr(parent_entity_v.c, entity_id)) \
+        .filter(getattr(parent_entity_v.c, entity_id).in_(entity_id_list.keys())) \
+        .outerjoin((Parent, getattr(Parent, parent_id) == getattr(parent_entity_v.c, parent_id))) \
+        .add_columns(getattr(Parent, parent_id), getattr(Parent, parent_desc)) \
         .distinct()
     
     results = []
-    for org_id, address_id, org_exists in query.all():
-        results.append((org_id, address_id, bool(org_exists), address_id_list[address_id]))
+    for parent_id, entity_id, parent_exists, parent_desc in query.all():
+        entity_id, entity_desc_new, entity_exists, entity_desc_old, user_name = entity_id_list[entity_id]
+        results.append((
+            parent_id, parent_desc, bool(parent_exists),
+            entity_id, entity_desc_new, entity_exists, entity_desc_old, user_name
+        ))
 
     return results
 
 
+
+def get_pending_org_address_id(orm):
+    return get_pending_parent_entity_id(
+        orm,
+        get_pending_address_id,
+        org_address_v,
+        Org, "org_id", "name", "address_id",
+    )
 
 def get_pending_org_contact_id(orm):
-    contact_id_list = dict(get_pending_entity_id(orm, Contact_v, Contact))
-
-    query = orm.query(org_contact_v.c.org_id, org_contact_v.c.contact_id) \
-        .filter(org_contact_v.c.contact_id.in_(contact_id_list.keys())) \
-        .outerjoin((Org, Org.org_id == org_contact_v.c.org_id)) \
-        .add_columns(Org.org_id) \
-        .distinct()
-    
-    results = []
-    for org_id, contact_id, org_exists in query.all():
-        results.append((org_id, contact_id, bool(org_exists), contact_id_list[contact_id]))
-
-    return results
-
-
+    return get_pending_parent_entity_id(
+        orm,
+        get_pending_contact_id,
+        org_contact_v,
+        Org, "org_id", "name", "contact_id",
+    )
 
 def get_pending_event_address_id(orm):
-    address_id_list = dict(get_pending_entity_id(orm, Address_v, Address))
-
-    query = orm.query(event_address_v.c.event_id, event_address_v.c.address_id) \
-        .filter(event_address_v.c.address_id.in_(address_id_list.keys())) \
-        .outerjoin((Event, Event.event_id == event_address_v.c.event_id)) \
-        .add_columns(Event.event_id) \
-        .distinct()
-    
-    results = []
-    for event_id, address_id, event_exists in query.all():
-        results.append((event_id, address_id, bool(event_exists), address_id_list[address_id]))
-
-    return results
-
-
+    return get_pending_parent_entity_id(
+        orm,
+        get_pending_address_id,
+        event_address_v,
+        Event, "event_id", "name", "address_id",
+    )
 
 def get_pending_event_contact_id(orm):
-    contact_id_list = dict(get_pending_entity_id(orm, Contact_v, Contact))
+    return get_pending_parent_entity_id(
+        orm,
+        get_pending_contact_id,
+        event_contact_v,
+        Event, "event_id", "name", "contact_id",
+    )
 
-    query = orm.query(event_contact_v.c.event_id, event_contact_v.c.contact_id) \
-        .filter(event_contact_v.c.contact_id.in_(contact_id_list.keys())) \
-        .outerjoin((Event, Event.event_id == event_contact_v.c.event_id)) \
-        .add_columns(Event.event_id) \
-        .distinct()
-    
-    results = []
-    for event_id, contact_id, event_exists in query.all():
-        results.append((event_id, contact_id, bool(event_exists), contact_id_list[contact_id]))
 
-    return results
+
 
 
 
--- handle/moderation.py
+++ handle/moderation.py
@@ -23,99 +23,125 @@ class ModerationQueueHandler(BaseHandler):
         if not self.moderator:
             raise HTTPError(404)
 
-        queue = {}
+        queue = {
+            "org": {},
+            "event": {},
+        }
 
-        queue["org"] = {}
-        for org_id, exists in get_pending_org_id(self.orm):
+
+        for org_id, desc_new, exists, desc_old, user_name in get_pending_org_id(self.orm):
             queue["org"][org_id] = {
                 "url": exists and "/organisation/%s" % org_id,
                 "revision_url": "/organisation/%s/revision" % org_id,
+                "description": desc_old or desc_new,
+                "user": user_name,
                 "address": {},
                 "contact": {},
                 }
 
-        for org_id, address_id, org_exists, address_exists in get_pending_org_address_id(self.orm):
+        for row in get_pending_org_address_id(self.orm):
+            org_id, org_desc, org_exists = row[:3]
+            address_id, address_desc_new, address_exists, address_desc_old, user_name = row[3:]
             if not org_id in queue["org"]:
                 if not org_exists:
                     continue
                 queue["org"][org_id] = {
                     "url": "/organisation/%s" % org_id,
                     "revision_url": None,
+                    "description": org_desc,
+                    "user": None,
                     "address": {},
                     "contact": {},
                     }
             address = {
                 "url": address_exists and "/address/%s" % address_id,
                 "revision_url": "/address/%s/revision" % address_id,
+                "description": address_desc_old or address_desc_new,
+                "user": user_name,
                 } 
             queue["org"][org_id]["address"][address_id] = address
 
-
-        for org_id, contact_id, org_exists, contact_exists in get_pending_org_contact_id(self.orm):
+        for row in get_pending_org_contact_id(self.orm):
+            org_id, org_desc, org_exists = row[:3]
+            contact_id, contact_desc_new, contact_exists, contact_desc_old, user_name = row[3:]
             if not org_id in queue["org"]:
                 if not org_exists:
                     continue
                 queue["org"][org_id] = {
                     "url": "/organisation/%s" % org_id,
                     "revision_url": None,
+                    "description": org_desc,
+                    "user": None,
                     "address": {},
                     "contact": {},
                     }
             contact = {
                 "url": contact_exists and "/contact/%s" % contact_id,
                 "revision_url": "/contact/%s/revision" % contact_id,
+                "description": contact_desc_old or contact_desc_new,
+                "user": user_name,
                 } 
             queue["org"][org_id]["contact"][contact_id] = contact
-                
 
-        queue["event"] = {}
-        for event_id, exists in get_pending_event_id(self.orm):
+        
+
+        for event_id, desc_new, exists, desc_old, user_name in get_pending_event_id(self.orm):
             queue["event"][event_id] = {
                 "url": exists and "/event/%s" % event_id,
                 "revision_url": "/event/%s/revision" % event_id,
+                "description": desc_old or desc_new,
+                "user": user_name,
                 "address": {},
                 "contact": {},
                 }
 
-        for event_id, address_id, event_exists, address_exists in get_pending_event_address_id(self.orm):
+        for row in get_pending_event_address_id(self.orm):
+            event_id, event_desc, event_exists = row[:3]
+            address_id, address_desc_new, address_exists, address_desc_old, user_name = row[3:]
             if not event_id in queue["event"]:
                 if not event_exists:
                     continue
                 queue["event"][event_id] = {
-                    "url": "/event/%s" % event_id,
+                    "url": "/eventanisation/%s" % event_id,
                     "revision_url": None,
+                    "description": event_desc,
+                    "user": None,
                     "address": {},
                     "contact": {},
                     }
             address = {
                 "url": address_exists and "/address/%s" % address_id,
                 "revision_url": "/address/%s/revision" % address_id,
+                "description": address_desc_old or address_desc_new,
+                "user": user_name,
                 } 
             queue["event"][event_id]["address"][address_id] = address
 
-        for event_id, contact_id, event_exists, contact_exists in get_pending_event_contact_id(self.orm):
+        for row in get_pending_event_contact_id(self.orm):
+            event_id, event_desc, event_exists = row[:3]
+            contact_id, contact_desc_new, contact_exists, contact_desc_old, user_name = row[3:]
             if not event_id in queue["event"]:
                 if not event_exists:
                     continue
                 queue["event"][event_id] = {
-                    "url": "/event/%s" % event_id,
+                    "url": "/eventanisation/%s" % event_id,
                     "revision_url": None,
+                    "description": event_desc,
+                    "user": None,
                     "address": {},
                     "contact": {},
                     }
             contact = {
                 "url": contact_exists and "/contact/%s" % contact_id,
                 "revision_url": "/contact/%s/revision" % contact_id,
+                "description": contact_desc_old or contact_desc_new,
+                "user": user_name,
                 } 
             queue["event"][event_id]["contact"][contact_id] = contact
+
 
         self.render(
             'moderation-queue.html',
             queue=queue,
             )
-
-
-
-        
-
 
