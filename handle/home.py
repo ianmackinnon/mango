f21dfbf37e5ff83c0e07cf17c303c5745118fc42
Ian Mackinnon <imackinnon@gmail.com>
2015-03-17 19:52:12 +0100

    Update arms fair pages: DSEI 2015, remove Farnborough 2014.

--- handle/home.py
+++ handle/home.py
@@ -118,14 +118,14 @@ class FairHandler(BaseHandler):
 
 class DseiHandler(FairHandler):
     name = "dsei"
-    tag_name = "dsei-2013"
+    tag_name = "dsei-2015"
 
 class FarnboroughHandler(FairHandler):
     name = "farnborough"
     tag_name = "farnborough-2014"
 
 class DseiTargetListHandler(HomeTargetListHandler):
-    tag_base = "dsei-2013"
+    tag_base = "dsei-2015"
 
 class FarnboroughTargetListHandler(HomeTargetListHandler):
     tag_base = "farnborough-2014"
